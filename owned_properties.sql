e9e909f77264758580a3c093358baf4c1a99e9db
Moochie <dh_moochie@yahoo.com>
2020-05-15 22:59:05 -0700

    Create owned_properties.sql

---
+++ owned_properties.sql
@@ -0,0 +1,40 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.10-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for drp
+CREATE DATABASE IF NOT EXISTS `drp` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `drp`;
+
+-- Dumping structure for table drp.owned_properties
+CREATE TABLE IF NOT EXISTS `owned_properties` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` int(11) DEFAULT NULL,
+  `char_id` int(11) DEFAULT NULL,
+  `keys` varchar(50) DEFAULT '{}',
+  `stash` varchar(50) DEFAULT NULL,
+  `mortgage_payments` int(11) NOT NULL DEFAULT 0,
+  `mortgage_amount` int(11) NOT NULL DEFAULT 0,
+  `last_payment` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `Index 2` (`char_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table drp.owned_properties: ~3 rows (approximately)
+/*!40000 ALTER TABLE `owned_properties` DISABLE KEYS */;
+/*!40000 ALTER TABLE `owned_properties` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
